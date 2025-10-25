3da15d3d2192447727875db21984d5b0a786b038
Panji Ahmad Nurhusni <panjinurhusni@gmail.com>
2025-10-26 03:00:25 +0700

    feat: change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const base = mode === "development" ? "/" : "/nurhusni-com/"; // GitHub Pages needs repo-based path in production
+  const base = "/" // GitHub Pages needs repo-based path in production
 
   return {
     base,
